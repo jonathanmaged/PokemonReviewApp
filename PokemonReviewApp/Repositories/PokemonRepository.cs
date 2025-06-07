a36475da811a6a870ebf7964a9d5055449ae33af
jonathan <jemmy01068619550@gmail.com>
2025-06-07 20:58:45 +0300

    feat: Add pokemon repository

---
+++ PokemonReviewApp/Repositories/PokemonRepository.cs
@@ -0,0 +1,21 @@
+﻿using PokemonReviewApp.Data;
+using PokemonReviewApp.Interfaces;
+using PokemonReviewApp.Models;
+
+namespace PokemonReviewApp.Repositories
+{
+    public class PokemonRepository : IPokemonRepository
+    {
+        private readonly DataContext _context
+            ;
+
+        public PokemonRepository(DataContext dataContext)
+        {
+            _context = dataContext;
+        }
+        public ICollection<Pokemon> GetPokemons()
+        {
+            return _context.Pokemons.OrderBy(p => p.Id).ToList();
+        }
+    }
+}
